0d8687dae0106c261692c1e158ba0b45bbae901a
unknown <dam206@ACARBALLEIRA.LOCAL>
2019-10-17 10:33:06 +0200

    Initial commit

---
+++ app/src/main/java/com/example/dam206/mislugaresbeatrizapp/AcercaDeActivity.java
@@ -0,0 +1,13 @@
+package com.example.dam206.mislugaresbeatrizapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AcercaDeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acerca_de);
+    }
+}
